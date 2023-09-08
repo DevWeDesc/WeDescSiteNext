a33549ee894098307b99837e6935d2e6ace1bd3a
ViniHGV <ixxvinicius@gmail.com>
2023-09-08 12:30:28 -0300

    Arrumando dimensão do contato

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -5,7 +5,7 @@ import { Image } from "next/dist/client/image-component";
 
 export const Contact = () => {
   return (
-    <div className="lg:w-[100%] xl-[80%] m-auto flex flex-col mt-20">
+    <div className="lg:w-[100%] w-full xl-[80%] m-auto flex flex-col mt-20">
       <div className="flex justify-center items-center gap-5 lg:py-10">
         <p className="w-[25%] h-[2px] bg-[#FECC09]"></p>
         <h2 className="text-4xl lg:text-5xl font-bold text-[#FECC09]">
@@ -19,7 +19,7 @@ export const Contact = () => {
         //   boxShadow: "0 0 40px rgba(123, 123, 123, 0.8)",
         // }}
       >
-        <div className="flex flex-col p-4 lg:p-10 gap-5">
+        <div className="flex flex-col w-full p-4 lg:p-10 gap-5">
           <p className="text-3xl font-bold">Entre em contato com a gente</p>
           <p className="text-xl pr-12 w-full">
             Conte com uma equipe com experiência em soluções de TI. Fale com
@@ -54,7 +54,7 @@ export const Contact = () => {
           </div>
         </div>
         <div className="w-[100px] h-[100px] 2xl:right-[47%] mt-[225%] z-[-1] lg:mt-[80px] rotate-45 bg-[#FECC09] absolute lg:right-[46.5%]"></div>
-        <div className="w-full bg-[#FECC09] py-10 px-12 lg:p-20 flex flex-col gap-2">
+        <div className="max-w-full bg-[#FECC09] py-10 px-6 lg:p-20 flex flex-col gap-2">
           <p className="text-2xl">Nome:</p>
           <input
             className="bg-transparent py-2 px-2 border-[1px] border-black outline-none rounded-lg"
