d194708f443129dc421034d323a048b3fa63faac
ViniHGV <ixxvinicius@gmail.com>
2023-09-11 08:51:38 -0300

    Arrumando espaçamento do background

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -43,22 +43,22 @@ export const About = () => {
         <Image
           alt=""
           src={LineImage}
-          className="w-screen absolute pt-[960px] z-[-2] lg:hidden"
+          className="w-screen absolute pt-[1060px] z-[-2] lg:hidden"
         ></Image>
         <Image
           alt=""
           src={ImageMobile}
-          className="w-screen absolute pt-[970px] z-[-2] lg:hidden"
+          className="w-screen absolute pt-[1070px] z-[-2] lg:hidden"
         ></Image>
         <Image
           alt=""
           src={ImageMobile}
-          className="w-screen absolute pt-[2750px] z-[-2] lg:hidden"
+          className="w-screen absolute pt-[2650px] z-[-2] lg:hidden"
         ></Image>
         <Image
           alt=""
           src={ImageMobile}
-          className="w-screen absolute pt-[4550px] z-[-2] lg:hidden"
+          className="w-screen absolute pt-[4450px] z-[-2] lg:hidden"
         ></Image>
         <Image
           alt=""
